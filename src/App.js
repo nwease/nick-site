819c99aa8d8cfbc4249cfc8fcd49f3d422973890
nwease <nickwease@gmail.com>
2020-05-04 11:15:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Cases from './components/Cases';
+import './scss/App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        let vh = window.innerHeight * .01;
+
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, [])
+
+    return (
+        <div className='App'>
+            <Header />
+            <Banner />
+            <Cases />
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ReactComponent as RightArrow } from '../assets/arrow-right.svg';
+
+const Banner = () => {
+    return (
+        <section className='main'>
+            <div className='container'>
+                <div className='row'>
+                    <h2>
+                        <div className='line'>
+                            <span>
+                                Creating dynamic brands is
+                            </span>
+                        </div>
+
+                        <div className='line'>
+                            <span>
+                                what we live for.
+                            </span>
+                        </div>
+                    </h2>
+
+                    <div className='btn-row'>
+                        <a href='/'>
+                            Learn more about us <RightArrow />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
